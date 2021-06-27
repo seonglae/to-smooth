044444fa1f8c710020c6240d10cd288cc9af9e7c
seonglae <sungle3737@gmail.com>
2021-06-27 17:44:10 +0900

    feat: dynamic dimension support

--- src/index.ts
+++ src/index.ts
@@ -11,17 +11,21 @@ function smooth(
   if (dimension && !path.every((point) => isMinSizeArray(point, dimension)))
     throw new Error('Dimension min size Error')
   while (iteration > 0) {
-    path = singlesmooth(path, factor)
+    path = singlesmooth(path, factor, dimension)
     iteration--
   }
   return path
 }
 
-function singlesmooth(points: Path = [], factor: number = 0.75) {
+function singlesmooth(
+  points: Path = [],
+  factor: number = 0.75,
+  dimension?: number
+) {
   const output = []
   if (points.length > 0) output.push(Array.from(points[0]))
   for (const i in points) {
-    const current = points[i]
+    const current = dimension ? points[i].slice(0, dimension) : points[i]
     const next = points[Number(i) + 1]
     if (!next) break
     const Q = current.map(
